05afede87b446d1a9a466417d2a0ec1b252360b9
Carlos A <54123771+Carlos96999@users.noreply.github.com>
2021-09-08 21:35:22 -0500

    Parte III Finalizado

--- src/main/java/org/ada/school/config/SecurityConfiguration.java
+++ src/main/java/org/ada/school/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity( securedEnabled = true, jsr250Enabled = true, prePostEnabled = true )
@@ -23,13 +24,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
     protected void configure( HttpSecurity http )
             throws Exception
     {
-        http.cors().and().csrf().disable()
-                .authorizeRequests()
-                .antMatchers( HttpMethod.GET, "/v1/health" ).permitAll()
-                .antMatchers( HttpMethod.POST,"/v1/auth" ).permitAll()
-                .antMatchers( HttpMethod.POST,"/v1/user" ).permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS );
+        http.addFilterBefore( jwtRequestFilter,
+                BasicAuthenticationFilter.class ).cors().and().csrf().disable().authorizeRequests().antMatchers(
+                HttpMethod.GET, "/v1/health" ).permitAll().antMatchers( HttpMethod.POST,
+                "/v1/auth" ).permitAll().anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS );
     }
 }
